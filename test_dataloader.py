55e474261ae21bcf6f2744a99df1d25351505217
nafisur <nafisur1990@gmail.com>
2025-06-02 16:38:57 +0200

    local changes

---
+++ test_dataloader.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("/home/nafisur/Documents/VASCO/custom_yolo/ultralytics/utralytics")  # Adjust path to import from parent directory
+
+from utralytics.data.dataloader import YOLODataset
